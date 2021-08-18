84d49fa63bad9626050a17ce74b69942ab01c1a3
PEDRO DOS SANTOS MONTEIRO SILVA <21192977@CFP127ALUNO.LOCAL>
2021-08-18 11:30:12 -0300

    Sqn! Acrescentando a tecla Enter como botão e destacando mensagens

--- professor.js
+++ professor.js
@@ -4,12 +4,13 @@ function calcularImc(peso, altura) {
 
 function classificar(imc) {
 
+    //let porque nesse caso varia, mudando o seu valor a cada condição
     let classificacao = ''
 
     if (imc < 18.5) {
         classificacao = 'abaixo do peso.'
     } else if (imc < 25) {
-        classificacao = 'com o peso ideal. Parabéns!'
+        classificacao = 'com o peso ideal. <span class="parabens">Parabéns!</span>'
     } else if (imc < 30) {
         classificacao = 'levemente acima do peso.'
     } else if (imc < 35) {
@@ -17,12 +18,13 @@ function classificar(imc) {
     } else if (imc < 40) {
         classificacao = 'com obesidade grau II.'
     } else {
-        classificacao = 'com obesidade grau III. Cuidado!'
+        classificacao = 'com obesidade grau III. <span class="cuidado">Cuidado!</span>'
     }
 
     return classificacao
 }
 
+//Verificando se os campos foram preenchidos
 function camposValidos() {
     return document.querySelector('form').reportValidity()
 }
@@ -37,11 +39,20 @@ function exibirResultado() {
         const imc = calcularImc(peso, altura)
         const classificacao = classificar(imc)
 
-        resultado.textContent = `${nome}, seu IMC é ${imc.replace('.', ',')} e você está ${classificacao}`
+        resultado.innerHTML = `${nome}, seu IMC é ${imc.replace('.', ',')} e você está ${classificacao}`
+        //innerHtml interpreta código html
     } else {
         resultado.textContent = 'Preencha todos os campos.'
     }
 }
 
+//dando à tecla "Enter" a mesma função do botão "Calcular"
+function capturarEnter(event) {
+    if (event.key == "Enter") {
+        exibirResultado()
+    }
+}
+
 // eventos
 document.getElementById('calcular').addEventListener('click', exibirResultado)
+document.querySelector('form').addEventListener('keypress', capturarEnter)
